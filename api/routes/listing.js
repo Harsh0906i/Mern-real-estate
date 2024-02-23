14358461a3af6414d808a7127ba5de5bbb6e2c73
Harshit <harshitsingharya24@gmail.com>
2024-02-23 11:56:53 +0530

    updatde

--- api/routes/listing.js
+++ api/routes/listing.js
@@ -104,7 +104,6 @@ router.get('/get', async (req, res, next) => {
             .sort({ [sort]: order })
             .limit(limit)
             .skip(startIndex);
-        console.log(listings)
         return res.status(200).json(listings);
     } catch (error) {
         next(error);
