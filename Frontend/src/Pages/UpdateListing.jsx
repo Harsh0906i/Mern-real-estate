1405fe573a10745087f655d960cdc7f1f8685e9a
Harshit <harshitsingharya24@gmail.com>
2024-02-23 11:32:24 +0530

    created about page

--- Frontend/src/Pages/UpdateListing.jsx
+++ Frontend/src/Pages/UpdateListing.jsx
@@ -179,7 +179,7 @@ export default function UpdateListing() {
                             <span>Bathroom</span>
                         </div>
                         <div className="flex items-center gap-2">
-                            <input type="number" min={'50'} max={'10000000'} onChange={handleChange} value={formData.regularPrice} className="p-3 border-gray-300 rounded-lg" id="regularPrice" required />
+                            <input type="number" min={'50'} max={'10000000000000'} onChange={handleChange} value={formData.regularPrice} className="p-3 border-gray-300 rounded-lg" id="regularPrice" required />
                             <div className="flex flex-col items-center">
                                 <p>Regular Price</p>
                                 <span className="text-xs">($ / month)</span>
@@ -187,7 +187,7 @@ export default function UpdateListing() {
                         </div>
                         {formData.offer && (
                             <div className="flex items-center gap-2">
-                                <input type="number" onChange={handleChange} value={formData.discountedPrice} className="p-3 border-gray-300 rounded-lg" min={'0'} max={'1000000'} id="discountedPrice" required />
+                                <input type="number" onChange={handleChange} value={formData.discountedPrice} className="p-3 border-gray-300 rounded-lg" min={'0'} max={'100000000000'} id="discountedPrice" required />
                                 <div className="flex flex-col items-center">
                                     <p>Discounted Price</p>
                                     <span className="text-xs">($ / month)</span>
